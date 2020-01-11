429d0d261f2d33845b2fb5e86597127badb76c54
purwar2804 <42232448+purwar2804@users.noreply.github.com>
2020-01-11 16:33:55 +0530

    Add files via upload

---
+++ christmas.py
@@ -0,0 +1,17 @@
+def translate(bilingual_dict,english_words_list):
+    #Write your logic here
+    swedish_words_list=[]
+    for i in range(0,len(english_words_list)):
+        swedish_words_list.append(bilingual_dict[english_words_list[i]])
+        
+
+    return swedish_words_list
+
+
+bilingual_dict= {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+english_words_list=["merry","christmas"]
+print("The bilingual dict is:",bilingual_dict)
+print("The english words are:",english_words_list)
+
+swedish_words_list=translate(bilingual_dict, english_words_list)
+print("The equivalent swedish words are:",swedish_words_list)
