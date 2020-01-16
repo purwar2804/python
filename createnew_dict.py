183d555c36fcb3525a9dd44dc35c4ea7eea8bcbb
UTKARSH PURWAR <42232448+purwar2804@users.noreply.github.com>
2020-01-16 15:55:21 +0530

    Add files via upload

---
+++ createnew_dict.py
@@ -0,0 +1,11 @@
+def create_new_dictionary(prices):
+    new_dict={}
+    for i in prices:
+        if(prices[i]>200):
+            new_dict[i]=prices[i]
+    return new_dict
+        
+        
+
+prices = { 'ACME': 45.23,'AAPL': 612.78,'IBM': 205.55,'HPQ': 37.20,'FB': 10.75}
+print(create_new_dictionary(prices))
