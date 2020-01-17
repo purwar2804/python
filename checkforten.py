e65bbbe520e0497fe6ca2ac561a529ebc9a577e4
UTKARSH PURWAR <42232448+purwar2804@users.noreply.github.com>
2020-01-17 16:20:57 +0530

    Add files via upload

---
+++ checkforten.py
@@ -0,0 +1,9 @@
+def check_for_ten(num1,num2):
+    if(num1==10 or num2==10):
+        return True
+    elif(num1+num2==10):
+        return True
+    else:
+        return False
+    
+print(check_for_ten(10,9))
